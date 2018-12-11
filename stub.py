13bac2905d945f1a3edb1a8dc6f1102138e30f47
Graham <4gra@users.noreply.github.com>
2018-12-11 23:40:43 +0000

    WIP daemon stuff
    just run in a loop for now, just playing

--- stub.py
+++ stub.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Very, very basic control of a PCLK-MN10 attached to a compatible hi-fi unit.
 #
-# This is not a script - it's my stream of consciousness / workbook.  
+# This is not a script - it's my stream of consciousness / workbook.
 # Tools might follow.
 #
 # These tools with errant parameters could quite feasibly cause permanent
@@ -23,13 +23,15 @@ except ImportError as err:
     print("Import error (%s): test mode only" % err, file=sys.stderr)
 
 # command list
-commandlist="commands.json"
-print_ascii=True
+commandlist = "commands.json"
+print_ascii = True
+
 
 class debug_pipe:
     """
     Returns two virtual USB pipes which simply relay communication to stdout.
     """
+
     def __init__(self, afile):
         self.afile = afile
 
@@ -45,14 +47,15 @@ def get_pipes():
     Performs device setup.
     Returns two USB endoints for (write, read).
     """
-    dev = usb.core.find(idVendor=0x054C,idProduct=0x0034)
+    dev = usb.core.find(idVendor=0x054C, idProduct=0x0034)
     if not dev:
         raise IOError("Cannot find PCLK-MN10 device")
 
     dev.set_configuration()
     cfg = dev.get_active_configuration()
     # print(cfg)
-    intf = cfg[(0,0)]
+    intf = cfg[(0, 0)]
+
     def finder(mask):
         return lambda e: usb.util.endpoint_direction(e.bEndpointAddress) == mask
 
@@ -60,30 +63,40 @@ def get_pipes():
             usb.util.find_descriptor(intf, custom_match=finder(usb.util.ENDPOINT_IN)))  # 0x82
     # print ("Found %s." % [ep, rep])
 
+
 def xprint(dat, pre="", asc=None):
     print("%s%s" % (pre, " ".join("{:02x}".format(x) for x in dat)))
     if asc or (asc == None and print_ascii == True):
-        print("TX|%s"%" ".join([ "%2s"%chr(x) if (x > 31 and x < 127) else '..' for x in dat ]))
+        print("TX|%s" % " ".join(
+            ["%2s" % chr(x) if (x > 31 and x < 127) else '..' for x in dat]))
+
 
 def hexin(instr):
     """converts a string or list of strings into commands"""
     for cmd in instr.split(","):
         yield [int(x, 16) for x in cmd.split(" ") if x]
 
+
 def jsend(dat, asc=None):
     """just send. and print."""
     xprint(dat, ">> ", asc)
     ep.write(dat)
 
-def jread(ll, delay=None, asc=None):
+
+def jread(ll, delay=None, asc=None, rtn=False):
     """just read <ll bytes>, optionally waiting <delay seconds> first."""
     if delay:
         time.sleep(delay)
     out = rep.read(ll)
+    acc = [out]
     while out:
         xprint(out, " < ", asc)
         out = rep.read(ll)
+        acc += [out]
     stdout.flush()
+    if rtn and acc:
+    	return acc
+
 
 def send(dat):
     """sends data and reads a max. of 40 bytes back."""
@@ -103,30 +116,32 @@ def vol_to_byte(lev):
         lev = 0
     elif lev == "MAX":
         lev = 31
-    return int(hex(int(lev,10) << 3),16)
+    return int(hex(int(lev, 10) << 3), 16)
+
 
 def byte_to_vol(vol):
     """
     Converts a volume output byte (as string) to a readable value
     """
-    return int(hex(int(vol,16) >> 3),16)
-    return int(vol,16)/8
+    return int(hex(int(vol, 16) >> 3), 16)
+    return int(vol, 16) / 8
 
 commands = {
 }
 
 # TODO: precompile this list once we are no longer
 # adding to it continually!
+
+
 def load_commands():
     if not commands:
         import json
         for path in [
             os.path.dirname(os.path.realpath(__file__)),
             os.getcwd(),
-            os.environ['HOME']
         ]:
             try:
-                with open(os.path.join(path,commandlist)) as f:
+                with open(os.path.join(path, commandlist)) as f:
                     #print("Attempting %s..." % os.path.join(path,commandlist),file=sys.stderr)
                     commands.update(json.load(f))
                     if commands:
@@ -136,6 +151,28 @@ def load_commands():
         print("Cannot load extra commands, continuing anyway.",file=sys.stderr)
     return commands
 
+# let's test a daemon reader
+import threading
+class LReader(threading.Thread):
+
+    def __init__(self, *args):
+    	super().__init__()
+    	self.params = args
+
+    def run(self):
+    	try:
+    		while True:
+	    		out = jread(*self.params)
+	    		for msg in [o for o in out if len(o)]:
+	    			length=msg[0]
+	    			typ=msg[4]
+		    		print("Message {:02x}, addr {:02x}{:02x}:{:02x}, Length {:02x}".format(
+		    			typ, msg[1], msg[2], msg[3], length)
+		    		)
+		    		if len(msg[5:]):
+		    			print(" ".join("{:02x}".format(x) for x in msg[5:]))
+    	except KeyboardInterrupt:
+    		return
 
 if __name__ == '__main__':
     from sys import argv, stdout, stderr
@@ -147,30 +184,46 @@ if __name__ == '__main__':
         try:
             (ep, rep) = get_pipes()
         except IOError as err:
-            print(err,file=stderr)
-            print("Use --test flag to continue without device.",file=stderr)
+            print(err, file=stderr)
+            print("Use --test flag to continue without device.", file=stderr)
             exit(1)
+
+    daemon = False
+    # -- clumsy arg parsing
+    if 'daemon' in argv[1:2]:
+        print("Daemon mode, ^C / ctrl-brk to quit after commands issued...", file=sys.stderr)
+        asc = ('--noascii' not in argv[1:])
+        daemon = LReader(32, 1.0, asc, True)
+        daemon.daemon = True
+        daemon.start()
+        argv = argv[1:]
+
     if "off" in argv[1:2]:
-        jread(32)
-        jsend([0x04,0x00,0x60,0xc0,0x2f])
+        if not daemon:
+            jread(64)
+        jsend([0x04, 0x00, 0x60, 0xc0, 0x2f])
         # TODO: take state
-        jread(32, 0.2)
+        if not daemon:
+            jread(32, 0.2)
     elif "on" in argv[1:2]:
-        send([0x00,0x60,0x00])
+        send([0x00, 0x60, 0x00])
         # TODO: take state
-        jread(32, 0.2)
+        if not daemon:
+            jread(32, 0.2)
     elif len(argv) > 2 and "vol" == argv[1]:
-        #TODO: if argv[2] == 'up'
-        #TODO: if argv[2] == 'down'
-        level=vol_to_byte(argv[2])
-        jread(32)
-        jsend([0x05,0x00,0x60,0xc0,0xc8]+[level])
-        jread(32)
+        # TODO: if argv[2] == 'up'
+        # TODO: if argv[2] == 'down'
+        level = vol_to_byte(argv[2])
+        if not daemon:
+            jread(32)
+        jsend([0x05, 0x00, 0x60, 0xc0, 0xc8] + [level])
+        if not daemon:
+            jread(32)
     elif "read" in argv[1:]:
         try:
-            ll=int(argv[2])
+            ll = int(argv[2])
         except:
-            ll=32
+            ll = 32
         finally:
             jread(ll)
     elif "asend" in argv[1:] and len(argv) > 2:
@@ -182,8 +235,9 @@ if __name__ == '__main__':
         for arg in argv[2:]:
             for x in hexin(arg):
                 send(x)
-                jread(32, 0.2)
-    elif argv[0][-2:] == 'poll' or "poll" in argv[1:]:
+                if not daemon:
+                    jread(32, 0.2)
+    elif (argv[0][-2:] == 'poll' or "poll" in argv[1:]) and not daemon:
         asc = ('--noascii' not in argv[1:])
         while True:
             jread(32, 0.1, asc)
@@ -208,7 +262,7 @@ if __name__ == '__main__':
             jread(32, 0.2)
             jread(32, 0.2)
     else:
-        ckeys=[x for x in load_commands().keys()]
+        ckeys = [x for x in load_commands().keys()]
         ckeys.sort()
         print("""\
 Usage: stub.py [--test] <command>
@@ -223,5 +277,7 @@ Where <command> is one of:
    (reads once per second)
 Or one of the named commands:
 %s
-""" % "\n".join(["  %s" % x for x in ckeys]),file=stderr)
+""" % "\n".join(["  %s" % x for x in ckeys]), file=stderr)
 
+    if daemon:
+        daemon.join()
